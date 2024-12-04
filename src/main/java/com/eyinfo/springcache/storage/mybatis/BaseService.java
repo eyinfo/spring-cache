f9696cb184b4e6ba82636b1554f4407b64a994bc
ethan <ljh0576123@163.com>
2024-12-04 10:52:49 +0800

    fix

--- src/main/java/com/eyinfo/springcache/storage/mybatis/BaseService.java
+++ src/main/java/com/eyinfo/springcache/storage/mybatis/BaseService.java
@@ -1,11 +1,13 @@
 package com.eyinfo.springcache.storage.mybatis;
 
 import com.eyinfo.foundation.entity.BaseEntity;
+import jakarta.annotation.Resource;
 
 public class BaseService<T extends BaseEntity, M extends ItemMapper<T>> {
 
     /**
      * mybatis mapper
      */
+    @Resource
     protected M itemMapper;
 }
