a4edea581a622ad0245ca880535a46fb3dee1c9f
ethan <ljh0576123@163.com>
2024-12-04 16:01:13 +0800

    base service删除itemMapper定义

--- src/main/java/com/eyinfo/springcache/storage/mybatis/BaseService.java
+++ src/main/java/com/eyinfo/springcache/storage/mybatis/BaseService.java
@@ -1,13 +1,7 @@
 package com.eyinfo.springcache.storage.mybatis;
 
 import com.eyinfo.foundation.entity.BaseEntity;
-import jakarta.annotation.Resource;
 
 public class BaseService<T extends BaseEntity, M extends ItemMapper<T>> {
 
-    /**
-     * mybatis mapper
-     */
-    @Resource
-    protected M itemMapper;
 }
